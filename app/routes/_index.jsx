a04b7c7348a2a612e6baf0cf2222ede7c09458c4
chrishannaby <chrishannaby@gmail.com>
2024-12-02 17:58:57 -0700

    fix data loading

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,8 +1,9 @@
-import {defer} from '@shopify/remix-oxygen';
+import {defer, json} from '@shopify/remix-oxygen';
 import {Await, useLoaderData, Link} from '@remix-run/react';
 import {Suspense} from 'react';
 import {Image, Money} from '@shopify/hydrogen';
 import {Content, fetchOneEntry} from '@builder.io/sdk-react';
+import {fetch as webFetch} from '@remix-run/web-fetch';
 
 const apiKey = '25b13b0802e6475f8d08239f389cd235';
 
@@ -25,177 +26,19 @@ export async function loader(args) {
     userAttributes: {
       urlPath,
     },
-  })
-  
-  return defer({...content});
-}
+    fetch: webFetch,
+  });
 
-/**
- * Load data necessary for rendering content above the fold. This is the critical data
- * needed to render the page. If it's unavailable, the whole page should 400 or 500 error.
- * @param {LoaderFunctionArgs}
- */
-async function loadCriticalData({context}) {
-  const [{collections}] = await Promise.all([
-    context.storefront.query(FEATURED_COLLECTION_QUERY),
-    // Add other queries here, so that they are loaded in parallel
-  ]);
-
-  return {
-    featuredCollection: collections.nodes[0],
-  };
-}
-
-/**
- * Load data for rendering content below the fold. This data is deferred and will be
- * fetched after the initial page load. If it's unavailable, the page should still 200.
- * Make sure to not throw any errors here, as it will cause the page to 500.
- * @param {LoaderFunctionArgs}
- */
-function loadDeferredData({context}) {
-  const recommendedProducts = context.storefront
-    .query(RECOMMENDED_PRODUCTS_QUERY)
-    .catch((error) => {
-      // Log query errors, but don't throw them so the page can still render
-      console.error(error);
-      return null;
-    });
-
-  return {
-    recommendedProducts,
-  };
+  return json({content});
 }
 
 export default function Homepage() {
   /** @type {LoaderReturnData} */
   const data = useLoaderData();
+  console.log(data);
   return (
     <div className="home">
       <Content content={data.content} apiKey={apiKey} model="page" />
     </div>
   );
 }
-
-/**
- * @param {{
- *   collection: FeaturedCollectionFragment;
-* }}
-*/
-function FeaturedCollection({collection}) {
- if (!collection) return null;
- const image = collection?.image;
- return (
-   <Link
-     className="featured-collection"
-     to={`/collections/${collection.handle}`}
-   >
-     {image && (
-       <div className="featured-collection-image">
-         <Image data={image} sizes="100vw" />
-       </div>
-     )}
-     <h1>{collection.title}</h1>
-   </Link>
- );
-}
-
-/**
-* @param {{
-*   products: Promise<RecommendedProductsQuery | null>;
-* }}
-*/
-function RecommendedProducts({products}) {
- return (
-   <div className="recommended-products">
-     <h2>Recommended Products</h2>
-     <Suspense fallback={<div>Loading...</div>}>
-       <Await resolve={products}>
-         {(response) => (
-           <div className="recommended-products-grid">
-             {response
-               ? response.products.nodes.map((product) => (
-                   <Link
-                     key={product.id}
-                     className="recommended-product"
-                     to={`/products/${product.handle}`}
-                   >
-                     <Image
-                       data={product.images.nodes[0]}
-                       aspectRatio="1/1"
-                       sizes="(min-width: 45em) 20vw, 50vw"
-                     />
-                     <h4>{product.title}</h4>
-                     <small>
-                       <Money data={product.priceRange.minVariantPrice} />
-                     </small>
-                   </Link>
-                 ))
-               : null}
-           </div>
-         )}
-       </Await>
-     </Suspense>
-     <br />
-   </div>
- );
-}
-
-const FEATURED_COLLECTION_QUERY = `#graphql
- fragment FeaturedCollection on Collection {
-   id
-   title
-   image {
-     id
-     url
-     altText
-     width
-     height
-   }
-   handle
- }
- query FeaturedCollection($country: CountryCode, $language: LanguageCode)
-   @inContext(country: $country, language: $language) {
-   collections(first: 1, sortKey: UPDATED_AT, reverse: true) {
-     nodes {
-       ...FeaturedCollection
-     }
-   }
- }
-`;
-
-const RECOMMENDED_PRODUCTS_QUERY = `#graphql
- fragment RecommendedProduct on Product {
-   id
-   title
-   handle
-   priceRange {
-     minVariantPrice {
-       amount
-       currencyCode
-     }
-   }
-   images(first: 1) {
-     nodes {
-       id
-       url
-       altText
-       width
-       height
-     }
-   }
- }
- query RecommendedProducts ($country: CountryCode, $language: LanguageCode)
-   @inContext(country: $country, language: $language) {
-   products(first: 4, sortKey: UPDATED_AT, reverse: true) {
-     nodes {
-       ...RecommendedProduct
-     }
-   }
- }
-`;
-
-/** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
-/** @template T @typedef {import('@remix-run/react').MetaFunction<T>} MetaFunction */
-/** @typedef {import('storefrontapi.generated').FeaturedCollectionFragment} FeaturedCollectionFragment */
-/** @typedef {import('storefrontapi.generated').RecommendedProductsQuery} RecommendedProductsQuery */
-/** @typedef {import('@shopify/remix-oxygen').SerializeFrom<typeof loader>} LoaderReturnData */
